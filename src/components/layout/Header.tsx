a55699dd2690d827d568696878713dd24276b3c9
jakubkanna <hello.jakubkanna@gmail.com>
2025-04-03 10:39:27 +0200

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -12,44 +12,31 @@ function Header() {
     <header>
       <Helmet>
         <title>{preferences?.artists_name}</title>
-        <meta name="title" content={preferences?.artists_name} />
         <meta name="author" content={preferences?.artists_name} />
-        <meta name="theme-color" content={lightColor} />
+      
         {/* Favicons and Icons for Different Platforms */}
-        <link rel="icon" href={`/favicon/favicon.ico`} type="image/x-icon" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href={`/favicon/favicon-32x32.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href={`/favicon/favicon-16x16.png`}
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`/favicon/apple-touch-icon.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href={`/favicon/android-chrome-192x192.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="512x512"
-          href={`/favicon/android-chrome-512x512.png`}
-        />
-
+        <link rel="icon" href="/favicon/favicon.ico" type="image/x-icon" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/favicon/android-chrome-512x512.png" />
+      
         {/* Optional: Web App Manifest for Android */}
-        <link rel="manifest" href={`/favicon/site.webmanifest`} />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      
+        {/* Open Graph Meta Tags for Social Media */}
+        <meta property="og:title" content={preferences?.artists_name} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/favicon/android-chrome-512x512.png" />
+        <meta property="og:url" content={window.location.href} />
+        
+        {/* Twitter Card Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={preferences?.artists_name} />
+        <meta name="twitter:image" content="/favicon/android-chrome-512x512.png" />
       </Helmet>
+
     </header>
   );
 }
