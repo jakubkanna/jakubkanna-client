074084e5cf39bfe79060182dbd15bda9ae1cd43c
jakubkanna <hello.jakubkanna@gmail.com>
2025-05-06 09:22:09 +0200

    Update Home.tsx

--- src/pages/single/Home.tsx
+++ src/pages/single/Home.tsx
@@ -129,7 +129,7 @@ function Homepage() {
         <title>JAKUB KANNA</title>
         <meta
           name="description"
-          content="Interdisciplinary artist, born in 1996 in Mrągowo (northeastern Poland). His practice blurs the boundaries between art, sport, and tech."
+          content="Interdisciplinary artist, born in 1996 in Mrągowo (northeastern Poland). His practice blurs the boundaries between art, sport, and technology."
         />
         <meta name="author" content="JAKUB KANNA" />
       </Helmet>
